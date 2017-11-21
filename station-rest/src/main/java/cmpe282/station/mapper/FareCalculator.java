7da331beaf9c438dc8eafda0bc7cde47b6d56fd3
hongyuanChrisLi <hychrisli@users.noreply.github.com>
2017-11-21 13:01:50 -0800

    Fare Calculator

--- station-rest/src/main/java/cmpe282/station/mapper/FareCalculator.java
+++ station-rest/src/main/java/cmpe282/station/mapper/FareCalculator.java
@@ -1,12 +1,26 @@
 package cmpe282.station.mapper;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
 
 public class FareCalculator {
 
-    public static Float calcFare (Date checkOutTs, Date checkinTs) {
+    private static final int BASE_MINUTES = 30;
+    private static final int INCR_MINUTES = 15;
+    private static final float BASE_RATE = 4.0f;	// half hour
+    private static final float INCR_RATE = 2.5f;	// every additional 15 minutes
+    protected static Logger LOGGER = Logger.getLogger(FareCalculator.class.getName());
+    
+    public static Float calcFare (Date checkoutTs, Date checkinTs) {
+	// LOGGER.info(checkoutTs.toString() + " --> " + checkoutTs.getTime());
+	// LOGGER.info(checkinTs.toString() + " --> " + checkinTs.getTime());
+	long diff = TimeUnit.MILLISECONDS.toMinutes(checkinTs.getTime() - checkoutTs.getTime());
+	// LOGGER.info("" + diff);
+	if ( diff <= 30 ) return BASE_RATE;
 	
-	return 0.0f;
+	return BASE_RATE + (( diff - BASE_MINUTES - 1 ) / INCR_MINUTES + 1) * INCR_RATE;
     }
     
 }
